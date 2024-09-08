b096bc7ba9de7fd2f56ddfec4a933188ec58273c
ZhafiraRamadhani <12350122936@students.uin-suska.ac.id>
2024-09-08 19:28:23 +0700

    Add files via upload

---
+++ ZhafiraRamadhani.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class ZhafiraRamadhani {
+    public static void main(String[] args) {
+        System.out.println("ZHAFIRA RAMADHANI");
+        System.out.println("12350122936");
+        System.out.println("Siapa yang bersungguh-sungguh akan berhasil. Siapa yang bersabar akan beruntung. Siapa yang berjalan di jalan-Nya akan sampai di tujuan.");
+    }
+}
